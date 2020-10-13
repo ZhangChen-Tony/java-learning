41775a8da6663f209cdd7064714e02909571ee0c
Zhang Chen Tony <tonyzhang3141@163.com>
2020-10-13 13:01:07 +0800

    update

--- basis/Variables.java
+++ basis/Variables.java
@@ -1,6 +1,8 @@
 public class Variables{
     public static void main(String[] args) {
-        int x = 10;
+        int x = 10; //赋值10
+        System.out.println(x);
+        x = 100;
         System.out.println(x);
     }
 }
