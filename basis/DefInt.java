f2c19f9ed3f09188c952e5430cc354fd393d57aa
Zhang Chen Tony <tonyzhang3141@163.com>
2020-10-14 10:56:36 +0800

    finalized basis, created basis/DefArray.java, and modified basis/DefInt java

--- basis/DefInt.java
+++ basis/DefInt.java
@@ -2,10 +2,10 @@ public class DefInt {
     public static void main(String[] args) {
         int i = 2147483647;
         int i2 = -2147483648;
-        int i3 = 2_000_000_000; /*  加下划线更容易识别*/
-        int i4 = 0xff0000;  /*十六进制表示的16711680*/
-        int i5 = 01000000000; /* 二进制表示的512*/
-        long l = 9000000000000000000L; /* long型的结尾需要加L*/
+        int i3 = 2_000_000_000; /**   加下划线更容易识别*/
+        int i4 = 0xff0000;  /**十六进制表示的16711680*/
+        int i5 = 01000000000; /**  二进制表示的512*/
+        long l = 9000000000000000000L; /** long型的结尾需要加L*/
         System.out.println(l);
         System.out.println(i);
         System.out.println(i2);
